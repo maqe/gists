b06e939d2d4c1b75cf443ac839234e66520d3dab
Jeff Dawson <jeff@fejalish.com>
2014-10-10 12:41:57 +0700

    Create image-loader.js

---
+++ Javascript/Ember/components/image-loader.js
@@ -0,0 +1,30 @@
+/*
+	
+	a image 'loader' component (based on http://ember.zone/a-graceful-image-loading-component/)
+
+	requires:
+		imagesLoaded (https://github.com/desandro/imagesloaded)
+		CSS styles
+	
+	can be used in HBS file like this: {{image-loader src=pictureUrl}}
+	
+*/
+
+Studi.ImageLoaderComponent = Ember.Component.extend({
+
+	classNames: ['img-wrapper'],
+	classNameBindings: ['loaded'],
+	src: '',
+	alt: '',
+	loaded: false,
+
+	handleLoad: function() {
+		var _self = this;
+		this.$().children('img').imagesLoaded()
+		.done(function(instance){
+			//remove loading style
+			_self.set('loaded', true);
+		})
+	}.on('didInsertElement')
+
+});
